60667e42fcead511ad76375c2a1a49bc115af1da
Christopher Mueller <chris.mueller@maritimeplanning.com>
2015-03-03 13:57:29 -0500

    Initial commit

---
+++ src/main/java/mpa/testci/SimpleStuff.java
@@ -0,0 +1,41 @@
+/*
+ * RPS ASA, Inc.
+ * Copyright(c) 2015. All Rights Reserved.
+ *
+ * SimpleStuff.java
+ *
+ * Created on Mar 3, 2015 @ 1:49:29 PM
+ */
+package mpa.testci;
+
+/**
+ *
+ * @author CBM <cmueller@asascience.com>
+ */
+public class SimpleStuff {
+
+    public static double arraySum(double... vals) {
+        double ret = vals[0];
+        for (int i = 1; i < vals.length; i++) {
+            ret += vals[i];
+        }
+        return ret;
+    }
+    
+    public static double arraySub(double... vals) {
+        double ret = vals[0];
+        for (int i = 1; i < vals.length; i++) {
+            ret -= vals[i];
+        }
+        return ret;
+    }
+    
+    public static double arrayProduct(double... vals) {
+        double ret = vals[0];
+        for (int i = 1; i < vals.length; i++) {
+            ret *= vals[i];
+        }
+        return ret;
+    }
+    
+}
---
+++ src/test/java/mpa/testci/SimpleStuffTest.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mpa.testci;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author CBM <cmueller@asascience.com>
+ */
+public class SimpleStuffTest {
+    
+    public SimpleStuffTest() {
+    }
+
+    /**
+     * Test of arraySum method, of class SimpleStuff.
+     */
+    @org.junit.Test
+    public void testArraySum() {
+        System.out.println("arraySum");
+        double[] vals = new double[]{2, 4, 8};
+        double expResult = 14;
+        double result = SimpleStuff.arraySum(vals);
+        assertEquals(expResult, result, 0.0);
+    }
+
+    /**
+     * Test of arraySub method, of class SimpleStuff.
+     */
+    @org.junit.Test
+    public void testArraySub() {
+        System.out.println("arraySub");
+        double[] vals = new double[]{8,4,3};
+        double expResult = 1;
+        double result = SimpleStuff.arraySub(vals);
+        assertEquals(expResult, result, 0.0);
+    }
+
+    /**
+     * Test of arrayProduct method, of class SimpleStuff.
+     */
+    @org.junit.Test
+    public void testArrayProduct() {
+        System.out.println("arrayProduct");
+        double[] vals = new double[]{2,4,8};
+        double expResult = 64;
+        double result = SimpleStuff.arrayProduct(vals);
+        assertEquals(expResult, result, 0.0);
+    }
+    
+}
